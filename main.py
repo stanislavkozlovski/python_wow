a9d3e008fa0b7dfc92fac30ee1273b23646d10c5
Enether <familyguyuser192@windowslive.com>
2016-08-08 01:52:51 +0300

    Added monster death, out of combat mode, game version and a welcome message

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+# TODO: Add command that shows all available commands
+# TODO: Handle Character death and health resets
+# TODO: Leave/Enter combat methods in classes
 # TODO: Add list with last twenty prints, clear the console and rewrite again whenever a command has been added
 # TODO: A million other things
 """
 current commands:
 attack - attacks the creature and ends the turn
 print stats - prints your health and the monster's. does not end the turn
+engage - start the fight
 """
+GAME_VERSION = '0.0.1 ALPHA'
 def main():
+    welcome_print()
     main_character = Character(name="Netherblood",
                                health=10,
                                mana=10,
@@ -16,26 +22,41 @@ def main():
                             mana=0,
                             min_damage=1,
                             max_damage=3)
-
+    alive_monsters = [test_creature]
     while True:
-        creature_swing = test_creature.deal_damage()
-        main_character.take_attack(creature_swing)
-        print("{0} attacks {1} for {2} damage!".format(test_creature.name, main_character.name, creature_swing))
+        print_live_monsters(alive_monsters)
 
         command = input()
-        while True:
-            if command == 'print stats':
-                print("Character {0} is at {1}/{2} health.".format(main_character.name, main_character.health, main_character.max_health))
-                print("Monster {0} is at {1}/{2} health".format(test_creature.name, test_creature.health, test_creature.max_health))
-                command = input()
-            else:
-                break
-        if command == 'attack':
-            character_dmg = main_character.deal_damage()
-            test_creature.take_attack(character_dmg)
-            print("{0} attacks {1} for {2} damage!".format(main_character.name, test_creature.name, character_dmg))
+        if command == 'engage':
+            main_character.in_combat = True
+
+        while main_character.in_combat:
+            creature_swing = test_creature.deal_damage()
+            main_character.take_attack(creature_swing)
+            print("{0} attacks {1} for {2} damage!".format(test_creature.name, main_character.name, creature_swing))
+
+            command = input()
+            while True:
+                if command == 'print stats':
+                    print("Character {0} is at {1}/{2} health.".format(main_character.name, main_character.health, main_character.max_health))
+                    print("Monster {0} is at {1}/{2} health".format(test_creature.name, test_creature.health, test_creature.max_health))
+                    command = input()
+                else:
+                    break
+            if command == 'attack':
+                character_dmg = main_character.deal_damage()
+                test_creature.take_attack(character_dmg)
+                print("{0} attacks {1} for {2} damage!".format(main_character.name, test_creature.name, character_dmg))
+
+            if not test_creature.alive:
+                main_character.in_combat = False
+                print("{0} has slain {1}".format(main_character.name, test_creature.name))
 
 
+def print_live_monsters(alive_monsters: list):
+    print("Alive monsters: ")
+    for i in range(len(alive_monsters)):
+        print(alive_monsters[i])
 
 class LivingThing:
     """
@@ -48,14 +69,15 @@ class LivingThing:
         self.mana = mana
 
 
-
-
 class Monster(LivingThing):
     def __init__(self, name: str, health: int=1, mana: int=1, min_damage: int=0, max_damage: int=1):
         super().__init__(name, health, mana)
         self.min_damage = min_damage
         self.max_damage = max_damage
         self.alive = True
+
+    def __str__(self):
+        return "Creature {0} - {1}/{2} HP | {3}/{4} Mana".format(self.name, self.health, self.max_health, self.mana, self.mana)
 
     def deal_damage(self):
         import random
@@ -81,6 +103,8 @@ class Weapon:
     def __init__(self, min_damage: int=0, max_damage: int=1):
         self.min_damage = min_damage
         self.max_damage = max_damage
+
+
 class Character(LivingThing):
     def __init__(self, name: str, health: int=1, mana: int=1, strength: int=1):
         super().__init__(name, health, mana)
@@ -89,6 +113,7 @@ class Character(LivingThing):
         self.max_damage = 1
         self.equipped_weapon = Weapon()
         self.alive = True
+        self.in_combat = False
 
     def equip_weapon(self, weapon: Weapon):
         self.equipped_weapon = weapon
@@ -118,5 +143,9 @@ class Character(LivingThing):
     def die(self):
         print("Character {} has been slain!".format(self.name))
 
+def welcome_print():
+    print("WELCOME TO PYTHON WOW VERSION: {0}".format(GAME_VERSION))
+    print("A simple console RPG game inspired by the Warcraft universe!")
+    print()
 if __name__ == '__main__':
     main()
