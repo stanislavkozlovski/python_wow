3315aea548b76194dcae6ef9d5d28d2de46f613f
Enether <familyguyuser192@windowslive.com>
2016-08-15 18:59:25 +0300

    Created an item_template table and an Item class inside items.py

--- items.py
+++ items.py
@@ -3,7 +3,13 @@ This holds the classes for every kind of item in the game
 """
 
 
-class Weapon:
+class Item:
+    def __init__(self, name: str):
+        self.name = name
+
+
+class Weapon(Item):
     def __init__(self, min_damage: int = 0, max_damage: int = 1):
+        super().__init__()
         self.min_damage = min_damage
         self.max_damage = max_damage
