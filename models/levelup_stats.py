f871fcd5d1d31cc678ff18bf0348fea8c5b39bae
Enether <familyguyuser192@windowslive.com>
2017-01-12 17:16:59 +0200

    Added a schema to levelup_stats

---
+++ models/levelup_stats.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer
+
+from database.main import Base
+
+
+class LevelUpStats(Base):
+    """
+    This table holds information about the amount of stats a character should gain according to the level he has attained
+    Example:
+        level, health, mana, strength, agility, armor
+            4,     10,   10,        7,       4,    15
+    A character who has just became level 4 will add 10hp, 10mana, 7strength, 4agiltiy and 15 armor to his stats
+        from the level up
+    """
+    __tablename__ = 'levelup_stats'
+
+    level = Column(Integer, primary_key=True)
+    health = Column(Integer)
+    mana = Column(Integer)
+    strength = Column(Integer)
+    agility = Column(Integer)
+    armor = Column(Integer)
