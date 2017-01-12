6b2800c04b803285fc2f6a924ce29c42b2866bbc
Enether <familyguyuser192@windowslive.com>
2017-01-12 17:19:17 +0200

    Added a schema to level_xp_requirement

---
+++ models/level_xp_requirement.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer
+
+from database.main import Base
+
+
+class LevelXpRequirement(Base):
+    """
+    This table holds information about the necessary XP needed to reach a certain level.
+    The table's contents are as follows:
+    level, xp_required(needed to reach the next one)
+    1,     400 (meaning you need to have 400XP to reach level 2)
+    2,     800 (800XP needed to reach level 3)
+    """
+    __tablename__ = 'level_xp_requirement'
+
+    level = Column(Integer, primary_key=True)
+    xp_required = Column(Integer)
