6a5f27c2b33dd4221c00bfd087287da1e9ea65d7
Enether <familyguyuser192@windowslive.com>
2017-01-14 12:16:01 +0200

    Restructured the models into separate folders

---
+++ models/main.py
@@ -0,0 +1,5 @@
+# Import every model out there to load the model itself
+from models.items import (item_template, loot_table)
+from models.misc import (level_xp_requirement, levelup_stats, quest_template, saved_character)
+from models.creatures import (creature_template, creature_defaults, creatures, npc_vendor)
+from models.spells import (paladin_spells_template, spell_dots, spell_buffs)
