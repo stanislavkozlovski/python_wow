d92fb1429b3cfb034890acaa526d42c1e674171b
Enether <familyguyuser192@windowslive.com>
2017-01-14 13:30:16 +0200

    Changed the imports in models/main to accomodate the new location of saved_character

--- models/main.py
+++ models/main.py
@@ -1,5 +1,6 @@
 # Import every model out there to load the model itself
 from models.items import (item_template, loot_table)
-from models.misc import (level_xp_requirement, levelup_stats, quest_template, saved_character)
+from models.misc import (level_xp_requirement, levelup_stats, quest_template)
+from models.characters import saved_character
 from models.creatures import (creature_template, creature_defaults, creatures, npc_vendor)
 from models.spells import (paladin_spells_template, spell_dots, spell_buffs)
