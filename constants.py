2f6490d6d6ff5f911d10943c8ae5dade64cccc36
Enether <familyguyuser192@windowslive.com>
2017-01-15 20:50:00 +0200

    renamed constants.py's creature default attributes variable

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 """ This file holds constant variables """
 from models.creatures.creature_defaults.loader import load_creature_defaults
 
-CREATURE_DEFAULT_ATTRIBUTES = load_creature_defaults()  # this function runs only once due to a decorator
+CREATURE_DEFAULT_VALUES = load_creature_defaults()  # this function runs only once due to a decorator
