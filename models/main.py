ca061e4d8013bc3a1f2ed49e456be713604dceb3
Enether <familyguyuser192@windowslive.com>
2017-01-15 20:48:19 +0200

    Re-ordered models/main.py's import statements

--- models/main.py
+++ models/main.py
@@ -2,6 +2,8 @@
 from models.items import (item_template, loot_table)
 from models.misc import (level_xp_requirement, levelup_stats)
 from models.quests import quest_template
+from models.creatures.creature_defaults import creature_defaults
+from models.creatures import (npc_vendor, creature_template, creatures)
 from models.characters import saved_character
-from models.creatures import (creature_template, creature_defaults, creatures, npc_vendor)
 from models.spells import (paladin_spells_template, spell_dots, spell_buffs)
+# WARNING: The order of which everything is imported is crucial!
