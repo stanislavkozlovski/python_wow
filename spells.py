98623e05862660877b75f67e2dad5cf4dc312368
Enether <familyguyuser192@windowslive.com>
2017-01-16 10:25:19 +0200

    Added the Paladin spell object

--- spells.py
+++ spells.py
@@ -2,10 +2,11 @@
 
 # The base class for spells
 class Spell:
-    def __init__(self, name: str, mana_cost: int=0, cooldown: int=0, beneficial_effect: 'BeneficialBuff'=None,
+    def __init__(self, name: str, rank: int, mana_cost: int=0, cooldown: int=0, beneficial_effect: 'BeneficialBuff'=None,
                  harmful_effect: 'DoT'=None):
         self.name = name
         self.mana_cost = mana_cost
+        self.rank = rank
         self.cooldown = cooldown
         self._cooldown_counter = 0
         self.is_ready = True  # boolean indicating if the spell is ready to be cast
@@ -15,11 +16,11 @@ class Spell:
     def __repr__(self):
         return f'Spell Object {self.name}: {self.mana_cost} Mana, {self._cooldown_counter}/{self.cooldown} CD.'
 
-    def cast(self, caster, target) -> int:
+    def cast(self) -> int:
         """
         Cast the spell, starting the cooldown counter and returning its mana cost
         """
-        if not (self._cooldown_counter == 0 and caster.has_enough_mana(self.mana_cost)):
+        if not self._cooldown_counter == 0:
             # TODO
             return
         self._cooldown_counter = self.cooldown
@@ -36,3 +37,21 @@ class Spell:
             self._cooldown_counter -= 1
         else:
             self.is_ready = True
+
+
+class PaladinSpell(Spell):
+    def __init__(self, name: str, rank: int, damage1: int=0, damage2: int=0, damage3: int=0, heal1: int=0, heal2: int=0,
+                 heal3: int=0, mana_cost: int=0, cooldown: int=0, beneficial_effect: 'BeneficialBuff' = None,
+                 harmful_effect: 'DoT' = None):
+        super().__init__(name, rank, mana_cost, cooldown, beneficial_effect, harmful_effect)
+        self.damage1 = damage1
+        self.damage2 = damage2
+        self.damage3 = damage3
+        self.heal1 = heal1
+        self.heal2 = heal2
+        self.heal3 = heal3
+
+    def __repr__(self):
+        return (f'Paladin Spell Object {self.name}: {self.mana_cost} Mana, {self._cooldown_counter}/{self.cooldown} CD.'
+                f'\nDamage values: {damage1} | {damage2} | {damage3} '
+                f'\nHeal values: {heal1} | {heal2} | {heal3}')
