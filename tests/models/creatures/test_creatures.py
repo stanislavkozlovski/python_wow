aaacc5e8cad9cd8d668812aa868a698d279e7023
Enether <familyguyuser192@windowslive.com>
2017-01-18 11:06:50 +0200

    Added a test for converting a Creatures schema to a Monster object

--- tests/models/creatures/test_creatures.py
+++ tests/models/creatures/test_creatures.py
@@ -24,7 +24,8 @@ class CreaturesTests(unittest.TestCase):
         self.monster_type = 'monster'
         self.monster_zone = 'Northshire Abbey'
         self.monster_subzone = 'A Peculiar Hut'
-        self.monster = Monster(monster_id=self.monster_entry, name="Brother Paxton", health=25, mana=0, armor=0, level=3,
+        self.monster_gold_range = range(5, 9)
+        self.monster = Monster(monster_id=self.monster_entry, name="Brother Paxton", health=25, mana=0, armor=80, level=3,
                                min_damage=7, max_damage=10, quest_relation_id=0, xp_to_give=100,
                                gold_to_give_range=(5, 8), loot_table=None, gossip="Nobody will foil our plans!")
 
@@ -39,6 +40,24 @@ class CreaturesTests(unittest.TestCase):
 
         self.assertIsNotNone(loaded_creature.creature)
         self.assertTrue(isinstance(loaded_creature.creature, CreatureTemplateSchema))
+
+    def test_convert_to_living_thing_object_monster(self):
+        """
+        Converting a CreaturesSchema with a type of 'monster' should convert it to a Monster object
+        """
+        converted_monster = session.query(CreaturesSchema).get(self.monster_guid).convert_to_living_thing_object()
+        self.assertTrue(isinstance(converted_monster, Monster))
+        # See that it has a loot table
+        self.assertTrue(isinstance(converted_monster.loot_table, LootTableSchema))
+        self.assertTrue(converted_monster.loot['gold'] in self.monster_gold_range)
+        # assert that the expected monster and the one we have are equal
+        converted_monster.loot_table = None  # remove the loot table for easier testing
+        # These things hold gold which is randomized per monster
+        converted_monster.loot = None
+        converted_monster._gold_to_give = None
+        self.monster.loot = None
+        self.monster._gold_to_give = None
+        self.assertEqual(vars(converted_monster), vars(self.monster))
 
 
 def tearDownModule():
