68375d5d083fede5a53cddbe38d118fec6810834
Enether <familyguyuser192@windowslive.com>
2017-01-23 13:08:06 +0200

    Test for monster/quest state preservation
    Moving in and back into a subzone should not reload monsters/quests

--- tests/zones/test_northshire_abbey.py
+++ tests/zones/test_northshire_abbey.py
@@ -52,7 +52,27 @@ class NorthshireAbbeyTests(unittest.TestCase):
         self.assertEqual(len(zone.cs_available_quests.keys()), self.northshire_vineyards_quest_count)
 
         self.assertEqual(zone.curr_subzone, 'Northshire Vineyards')
-        pass
+
+    def test_kill_monsters_complete_quests_move_player_move_back(self):
+        """
+        Kill the monsters and complete the quests in a subzone, move to another subzone and come back
+        Assert that the monsters/quests have not been loaded again and have save their previous state
+        """
+        zone = NorthshireAbbey(self.char_mock)
+        start_subzone, go_to_subzone = 'Northshire Valley', 'Northshire Vineyards'
+        # assert that the values are as expected
+        self.assertEqual(zone.curr_subzone, 'Northshire Valley')
+        self.assertEqual(len(zone.cs_alive_monsters.keys()), self.northshire_valley_monster_count)
+        self.assertEqual(len(zone.cs_available_quests.keys()), self.northshire_valley_quest_count)
+        # simulate killing all the monsters and completing the quests
+        zone.cs_alive_monsters = {}
+        zone.cs_monsters_guid_name_set = set()
+        zone.cs_available_quests = {}
+
+        zone.move_player(current_subzone=start_subzone, destination=go_to_subzone, character=self.char_mock)
+        zone.move_player(current_subzone=go_to_subzone, destination=start_subzone, character=self.char_mock)
+        self.assertEqual(len(zone.cs_alive_monsters.keys()), 0)
+        self.assertEqual(len(zone.cs_available_quests.keys()), 0)
 
 if __name__ == '__main__':
     unittest.main()
