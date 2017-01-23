44ddeba4834086aac3897cc8d093f9dd1ec090b1
Enether <familyguyuser192@windowslive.com>
2017-01-23 09:55:09 +0200

    Added a test for the convert_to_paladin_spell_object func

--- tests/models/spells/test_paladin_spells.py
+++ tests/models/spells/test_paladin_spells.py
@@ -24,9 +24,10 @@ class PaladinSpellsSchemaTests(unittest.TestCase):
         """
         self.spell_entry = 4
         self.spell_name = 'Melting Strike'
+        self.dot = DoT(name='Melting', damage_tick=2, duration=2, caster_lvl=0)
         self.expected_spell = PaladinSpell(name=self.spell_name, rank=1, damage1=3, damage2=0, damage3=0,
                                            heal1=0, heal2=0, heal3=0, mana_cost=6, cooldown=3,
-                                           beneficial_effect=None, harmful_effect=None)
+                                           beneficial_effect=None, harmful_effect=self.dot)
 
     def test_schema_values(self):
         """ Load a schema object and assert that every value is as expected"""
@@ -50,7 +51,12 @@ class PaladinSpellsSchemaTests(unittest.TestCase):
         self.assertIsNone(loaded_schema.buff)
         self.assertTrue(isinstance(loaded_schema.dot, DotSchema))
 
-
+    def test_convert_to_paladin_spell_object(self):
+        """
+        See if the convert_to_paladin_spell_object works properly
+        """
+        loaded_quest: PaladinSpell = session.query(PaladinSpellsSchema).get(self.spell_entry).convert_to_paladin_spell_object()
+        self.assertEqual(vars(loaded_quest), vars(self.expected_spell))
 
 
 def tearDownModule():
