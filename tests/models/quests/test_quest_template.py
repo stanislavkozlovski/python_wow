f33975672d48a0b75027f5ef65658369af177334
Enether <familyguyuser192@windowslive.com>
2017-01-23 08:43:58 +0200

    Test for the Quest Schema

---
+++ tests/models/quests/test_quest_template.py
@@ -0,0 +1,67 @@
+import unittest
+
+from tests.delete_test_db import delete_test_db  # module that deletes the DB :)
+import database.main
+from tests.create_test_db import engine, session, Base
+
+
+database.main.engine = engine
+database.main.session = session
+database.main.Base = Base
+import models.main
+from models.quests.quest_template import QuestSchema
+from models.items.item_template import ItemTemplateSchema
+from quest import KillQuest, FetchQuest, Quest
+
+
+class CreatureTemplateTests(unittest.TestCase):
+    def setUp(self):
+        """
+        There are two type of quests for now - KillQuest and FetchQuest.
+        Test both of them
+        """
+        self.quest_entry = 1
+        self.name = 'A Canine Menace'
+        self.type = 'killquest'
+        self.level_required = 1
+        self.monster_required = 'Wolf'
+        self.item_required = None
+        self.amount_required = 5
+        self.zone = 'Northshire Abbey'
+        self.subzone = 'Northshire Valley'
+        self.xp_reward = 300
+        self.description = 'Kill 5 Wolves'
+        self.item_reward1 = None
+        self.item_reward2 = None
+        self.item_reward3 = None
+        self.item_choice_enabled = False
+
+    def test_values(self):
+        received_q_schema: QuestSchema = session.query(QuestSchema).get(self.quest_entry)
+        self.assertTrue(isinstance(received_q_schema.entry, int))
+        self.assertTrue(isinstance(received_q_schema.name, str))
+        self.assertTrue(isinstance(received_q_schema.type, str))
+        self.assertTrue(isinstance(received_q_schema.level_required, int))
+        self.assertTrue(isinstance(received_q_schema.monster_required, str))
+        self.assertIsNone(received_q_schema.item_required)
+        self.assertTrue(isinstance(received_q_schema.amount_required, int))
+        self.assertTrue(isinstance(received_q_schema.zone, str))
+        self.assertTrue(isinstance(received_q_schema.sub_zone, str))
+        self.assertTrue(isinstance(received_q_schema.xp_reward, int))
+        self.assertTrue(isinstance(received_q_schema.description, str))
+        self.assertIsNone(received_q_schema.reward1_id)
+        self.assertIsNone(received_q_schema.reward2_id)
+        self.assertIsNone(received_q_schema.reward3_id)
+        self.assertTrue(isinstance(received_q_schema.item_choice_enabled, bool))
+        # since there are no items as reward these should be none
+        self.assertIsNone(received_q_schema.reward1)
+        self.assertIsNone(received_q_schema.reward2)
+        self.assertIsNone(received_q_schema.reward3)
+
+
+
+def tearDownModule():
+    delete_test_db()
+
+if __name__ == '__main__':
+    unittest.main()
