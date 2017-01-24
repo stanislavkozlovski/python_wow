43dc24e0e2aa08010d2702f3bf9e55590ea4ced1
Enether <familyguyuser192@windowslive.com>
2017-01-24 14:46:17 +0200

    Test for the add_buff function

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -2,6 +2,7 @@ import unittest
 
 from constants import KEY_ARMOR_ATTRIBUTE
 from entities import LivingThing
+from buffs import BeneficialBuff
 
 
 class LivingThingTests(unittest.TestCase):
@@ -10,6 +11,7 @@ class LivingThingTests(unittest.TestCase):
         self.health = 100
         self.mana = 100
         self.level = 5
+        self.dummy_buff = BeneficialBuff(name='Zdrave', buff_stats_and_amounts=[('health', self.health)], duration=5)
         self.expected_attributes = {KEY_ARMOR_ATTRIBUTE: 0}  # the base stat for every creature
         self.dummy = LivingThing(name=self.name, health=self.health, mana=self.mana, level=self.level)
 
@@ -44,6 +46,20 @@ class LivingThingTests(unittest.TestCase):
         # Should have left combat and regenerated health
         self.assertFalse(self.dummy.is_in_combat())
         self.assertEqual(self.dummy.health, self.health)
+
+    def test_add_buff(self):
+        """
+        The add_buff method should add a DoT/BeneficialBuff to the character's self.buffs dictionary.
+        It should also apply the buff's stats to the character (if it's a BeneficialBuff)
+        """
+        self.assertEqual(self.dummy.buffs, {})
+        self.dummy.add_buff(self.dummy_buff)
+        self.assertEqual(self.dummy.buffs, {self.dummy_buff: self.dummy_buff.duration})
+        # check if the buff has been applied
+        # because the buff has been applied out of combat, his current health should get buffed
+        self.assertNotEqual(self.dummy.health, self.health)
+        self.assertEqual(self.dummy.max_health, self.health + self.dummy_buff.buff_amounts['health'])
+        self.assertEqual(self.dummy.health, self.health + self.dummy_buff.buff_amounts['health'])
 
 
 
