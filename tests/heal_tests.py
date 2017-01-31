a95eddca94c9ca51972d75db89c4d9da43350871
Enether <familyguyuser192@windowslive.com>
2017-01-31 16:38:36 +0200

    Test for __add__

--- tests/heal_tests.py
+++ tests/heal_tests.py
@@ -11,6 +11,16 @@ class HealTests(unittest.TestCase):
 
         self.assertEqual(str(heal), expected_str)
 
+    def test_add(self):
+        """ __add__ takes a numeric type (int/float) """
+        heal_amount = 5
+        heal = Heal(heal_amount=heal_amount)
+
+        add_amount = 10
+        expected_amount = heal_amount + add_amount
+
+        self.assertEqual(heal + add_amount, expected_amount)
+
 
 if __name__ == '__main__':
     unittest.main()
