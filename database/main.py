5f5a475ef16207163f4bdeecc76ff54a0d76668c
MRRollMan <konkow7@gmail.com>
2019-10-30 01:53:27 +0200

    Fixed open database in windows

--- database/main.py
+++ database/main.py
@@ -4,7 +4,7 @@ from database.database_info import DB_PATH
 
 from sqlalchemy.orm import sessionmaker
 
-engine = sqlalchemy.create_engine(f'sqlite:////{DB_PATH}')
+engine = sqlalchemy.create_engine(f'sqlite:///{DB_PATH}')
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
